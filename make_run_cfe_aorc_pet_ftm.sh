d277467f85a1b552f1203e5bdc5afd3171162fff
Ahmad Jan <ajkhattak@gmail.com>
2021-12-20 10:45:12 -0500

    reset .sh file to default config files.

--- make_run_cfe_aorc_pet_ftm.sh
+++ make_run_cfe_aorc_pet_ftm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#_build/cfeft ./configs/laramie_bmi_config_cfe_pass.txt ./configs/laramie_bmi_config_aorc.txt ./configs/laramie_bmi_config_pet_pass.txt ./configs/laramie_bmi_config_ftm.txt
-_build/cfeft ./configs/laramie_bmi_config_cfe_pass.txt ./configs/laramie_bmi_config_aorc.txt ./configs/laramie_bmi_config_pet_pass.txt ./configs/laramie_bmi_config_ftm_res.txt
+_build/cfeft ./configs/laramie_bmi_config_cfe_pass.txt ./configs/laramie_bmi_config_aorc.txt ./configs/laramie_bmi_config_pet_pass.txt ./configs/laramie_bmi_config_ftm.txt
 
-#_build/cfeft ./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt ./configs/cat_87_bmi_config_pet_pass.txt ./configs/cat_87_bmi_config_ftm.txt
 
 
 
