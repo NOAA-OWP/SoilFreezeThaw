831e9c0f956af31142de0ebc1cc3f52e85c9cbde
Ahmad Jan <ajkhattak@gmail.com>
2022-02-14 18:03:06 -0500

    cfe should be pulled from its own repo. cleanup from the freeze-thaw repo.

--- make_and_run_bmi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -lm -Wall ./src/main.c ./src/bmi_cfe.c ./src/cfe.c -o run_cfe_bmi
-./run_cfe_bmi ./configs/cat_87_bmi_config_cfe.txt
--- make_run_cfe_aorc_ftm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-_build/cfeft ./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt ./configs/cat_87_bmi_config_ftm.txt
