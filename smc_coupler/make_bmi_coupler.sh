e6c162e7db64781b8d8c9ae3b04a8171967cd607
Ahmad Jan <ajkhattak@gmail.com>
2022-03-07 17:14:28 -0500

    removed smc_coupler, now clone from its own repo.

--- smc_coupler/make_bmi_coupler.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-g++ -lm -Wall -O -g ./src/bmi_main.cxx ./src/bmi_coupler.cxx ./src/smc_profile.cxx -o run_bmi_coupler
-#${CXX} -lm -Wall -O -g ./src/bmi_main.cxx ./src/bmi_coupler.cxx ./src/smc_profile.cxx -o run_bmi_coupler
