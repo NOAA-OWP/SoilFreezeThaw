d931e585d5d923000621927176e5161db99600b2
Austin Raney <austin.raney@noaa.gov>
2023-12-21 11:21:44 -0500

    Compile unit tests with c++11

--- tests/run_unittest.sh
+++ tests/run_unittest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-${CXX} -lm -Wall -O -g ./main_unittest.cxx ../src/bmi_soil_freeze_thaw.cxx ../src/soil_freeze_thaw.cxx -o run_sft
+${CXX} -std=c++11 -lm -Wall -O -g ./main_unittest.cxx ../src/bmi_soil_freeze_thaw.cxx ../src/soil_freeze_thaw.cxx -o run_sft
 ./run_sft configs/unittest.txt
 rm -f run_sft
 rm -rf run_sft.dSYM
